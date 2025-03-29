9a7f4d0ceeda8bf3de00101d1ce4df01246fb8cb
Sravya bandi <sravyabandi811@gmail.com>
2025-03-29 12:43:31 +0530

    Update RoundRobinSchduling.c

--- RoundRobinSchduling.c
+++ RoundRobinSchduling.c
@@ -1,56 +1,59 @@
-#include<stdio.h>
-int main(){
-        int pid[15],tt=0,t,bt[15],rt[15],r=0,n,p=0,c,at[15],ct[15],tat[15],wt[15],s=0;
-        float avg;
-        printf("Enter Number of Process :: ");
-        scanf("%d",&n);
-        printf("Enter %d Process Ids :: \n",n);
-        for (int i = 0; i < n; i++)
-        {
-        scanf("%d",&pid[i]);
-        }
-        for(int i=0; i < n;i++){
-                printf("Enter process %d Burst Time :: ",pid[i]);
-                scanf("%d",&bt[i]);
-        }
-        printf("Enter the Time Quantam :: ");
-        scanf("%d",&t);
-        for(int i=0;i<n;i++){
-            tt=tt+bt[i];
-            rt[i]=bt[i];
-        }
-        ct[0]=0;
-        while (r<tt)
-        {
-        for(int i=0;i<n;i++){
-                if(rt[i]!=0){
-                         if(rt[i]<t){
-                                ct[i]=ct[p]+rt[i];
-                                p=i;
-                                r=r+rt[i];
-                                rt[i]=0;
-                                }
-                         else{
-                                ct[i]=ct[p]+t;
-                                rt[i]=rt[i]-t; 
-                                p=i; 
-                                r=r+t;     
-                                }
+#include <stdio.h>
+int main() {
+    int i, j, n, bu[10], wa[10], tat[10], t, ct[10], remaining;
+    float awt = 0, att = 0, temp = 0;
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+        printf("Enter Burst Time for process %d: ", i + 1);
+        scanf("%d", &bu[i]);
+        ct[i] = bu[i]; // Store original burst time
+    }
+    printf("Enter the time quantum: ");
+    scanf("%d", &t);
+    remaining = n; // Track remaining processes
+    // Round Robin Scheduling
+    while (remaining > 0) {
+        for (i = 0; i < n; i++) {
+            if (bu[i] > 0) {
+                if (bu[i] <= t) { // Process completes in this cycle
+                    temp += bu[i];
+                    tat[i] = temp; // Turnaround time
+                    bu[i] = 0;
+                    remaining--; // Process finished
+                } else { // Process still has remaining burst time
+                    bu[i] -= t;
+                    temp += t;
                 }
+            }
         }
-        }
-                for(int i=0;i<n;i++){
-                        wt[i]=ct[i]-bt[i];
-                        s=s+wt[i];
-                }
-        printf("|        Process Id       |       Burst Time       |       Complete Time      |       Waiting Time      |\n");
-        printf("|-------------------------|------------------------|--------------------------|-------------------------|\n");
-        for(int i=0;i<n;i++){
-                printf("|%25d|%24d|%26d|%25d|\n",pid[i],bt[i],ct[i],wt[i]);
-        }
-        avg=(float)s/n;
-        printf("Average Waiting Time using Round Robin Technique :: %.2f",avg);
-        
-        printf("\n"); 
-        return 0;
+    }
+    // Calculate waiting times
+    for (i = 0; i < n; i++) {
+        wa[i] = tat[i] - ct[i]; // Waiting time = Turnaround time - Burst time
+        awt += wa[i];
+        att += tat[i];
+    }
+    // Print results
+    printf("\nThe Average Turnaround Time is: %.2f", att / n);
+    printf("\nThe Average Waiting Time is: %.2f\n", awt / n);
+    
+    printf("\n\tPROCESS\t BURST TIME\t WAITING TIME\t TURNAROUND TIME\n");
+    for (i = 0; i < n; i++) {
+        printf("\t%d\t %d\t\t %d\t\t %d\n", i + 1, ct[i], wa[i], tat[i]);
+    }
+    return 0;
 }
+
+// Output
+// Enter the number of processes: 3
+// Enter Burst Time for process 1: 24
+// Enter Burst Time for process 2: 3
+// Enter Burst Time for process 3: 3
+// Enter the time quantum: 3
+// The Average Turnaround Time is: 15.00
+// The Average Waiting Time is: 5.00
+// 	PROCESS	 BURST TIME	 WAITING TIME	 TURNAROUND TIME
+// 	1	 24		 6		 30
+// 	2	 3		 3		 6
+// 	3	 3		 6		 9
